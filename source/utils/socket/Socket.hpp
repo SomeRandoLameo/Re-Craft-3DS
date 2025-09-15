f45c94df849f04a1589465a0e9bceb686ba4524b
Team Omega <100494326+EinfachEli08@users.noreply.github.com>
2025-09-15 18:01:44 +0200

    Moved functions out of Socket

--- source/utils/socket/Socket.hpp
+++ source/utils/socket/Socket.hpp
@@ -16,11 +16,7 @@ public:
     static void initSOC();
     static void shutdownSOC();
 
-    static std::vector<std::string> parseHost(const std::string& host);
-    static int parseIntWithDefault(const std::string& str, int defaultValue);
     static bool isAllowedChar(char c);
-    static std::string readString(int sock, int maxLength);
-    static int utf16be_to_utf8(const uint8_t *in, int inLen, char *out, int outSize);
 };
 
 #endif
