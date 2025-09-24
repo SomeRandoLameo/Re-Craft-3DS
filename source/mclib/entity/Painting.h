9d922f510bac7d51e39d0ec0ee78f575c43144c3
Team Omega <100494326+EinfachEli08@users.noreply.github.com>
2025-09-24 15:39:29 +0200

    Coverted Entity Headers

---
+++ source/mclib/entity/Painting.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "Entity.h"
+
+namespace mc {
+    namespace entity {
+
+        class PaintingEntity : public Entity {
+        public:
+            enum class Direction {
+                South, West, North, East
+            };
+        private:
+            std::wstring m_Title;
+            Direction m_Direction;
+
+        public:
+            PaintingEntity(EntityId eid, protocol::Version protocolVersion) : Entity(eid, protocolVersion), m_Title(L""), m_Direction(Direction::South) { SetType(EntityType::Painting); }
+
+            const std::wstring& GetTitle() const noexcept { return m_Title; }
+            Direction GetDirection() const noexcept { return m_Direction; }
+
+            inline void SetTitle(const std::wstring& title) { m_Title = title; }
+            inline void SetDirection(Direction dir) noexcept { m_Direction = dir; }
+        };
+
+    } // ns entity
+} // ns mc
