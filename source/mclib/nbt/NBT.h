797f8a88fcd197d6c4469307df74b98d93507ddb
Team Omega <100494326+EinfachEli08@users.noreply.github.com>
2025-09-24 12:29:47 +0200

    Added NBT from mclib

---
+++ source/mclib/nbt/NBT.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "../mclib.h"
+#include "../common/Types.h"
+#include "Tag.h"
+
+#include <string>
+#include <vector>
+#include <memory>
+#include <map>
+
+namespace mc {
+
+    class DataBuffer;
+
+    namespace nbt {
+
+        class NBT {
+        private:
+            TagCompound m_Root;
+
+        public:
+            MCLIB_API NBT() { }
+
+            NBT(const NBT& rhs) = default;
+            NBT& operator=(const NBT& rhs) = default;
+            NBT(NBT&& rhs) = default;
+            NBT& operator=(NBT&& rhs) = default;
+
+            TagCompound& GetRoot() noexcept { return m_Root; }
+            const TagCompound& GetRoot() const noexcept { return m_Root; }
+            bool HasData() const { return m_Root.begin() != m_Root.end(); }
+
+            void SetRoot(const TagCompound& compound) { m_Root = compound; }
+
+            template <typename T>
+            T* GetTag(const std::wstring& tagName) const {
+                return m_Root.GetTag<T>(tagName);
+            }
+
+            friend MCLIB_API DataBuffer& operator>>(DataBuffer& out, NBT& nbt);
+        };
+
+        MCLIB_API DataBuffer& operator<<(DataBuffer& out, const NBT& nbt);
+        MCLIB_API DataBuffer& operator>>(DataBuffer& in, NBT& nbt);
+
+    } // ns nbt
+} // ns mc
