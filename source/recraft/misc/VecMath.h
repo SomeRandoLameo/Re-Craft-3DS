2584aa0520649e3aee6cde1458ce7f227d9bd8db
elias <einfacheli.games@gmail.com>
2025-10-29 22:14:33 +0100

    Attempt to migrate VecMath to MCLib

--- source/recraft/misc/VecMath.h
+++ source/recraft/misc/VecMath.h
@@ -3,6 +3,8 @@
 #include <math.h>
 
 #include "NumberUtils.h"
+
+#include "../../mclib/common/Vector.h"
 //TODO: Use MCLIB Vector stuff
 typedef union {
 	float v[3];
@@ -15,7 +17,11 @@ static inline float3 f3_new(float x, float y, float z) { return (float3){{x, y, 
 
 static inline float3 f3_add(float3 a, float3 b) { return f3_new(a.x + b.x, a.y + b.y, a.z + b.z); }
 static inline float3 f3_sub(float3 a, float3 b) { return f3_new(a.x - b.x, a.y - b.y, a.z - b.z); }
+
+//TODO: BIG FAT TODO HERE
 static inline float3 f3_scl(float3 a, float b) { return f3_new(a.x * b, a.y * b, a.z * b); }
+
+static inline mc::Vector3f Vector3f_Scale(mc::Vector3f a, float b) { return mc::Vector3f(a.x * b, a.y * b, a.z * b); }
 
 static inline float f3_dot(float3 a, float3 b) { return a.x * b.x + a.y * b.y + a.z * b.z; }
 static inline float3 f3_crs(float3 a, float3 b) { return f3_new(a.y * b.z - a.z * b.y, a.z * b.x - a.x * b.z, a.x * b.y - a.y * b.x); }
