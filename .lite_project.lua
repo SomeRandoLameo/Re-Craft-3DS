3f579e9487013e94b2897ea331b5bbab523d98a9
Moddimation <vakhidmytr06@gmail.com>
2026-01-27 15:38:12 +0100

    fixes

--- .lite_project.lua
+++
@@ -1,8 +0,0 @@
-local config = require "core.config"
-
-config.plugins.build.targets = {
-  { name = "Re-Craft-3DS_3dsx", build_directory = "build", type = "cmake" },
-  { name = "send", build_directory = "build", type = "cmake" }
-}
-config.plugins.debugger.remote_ip = "192.168.178.60:4003"
-
--- include/recraft/gui/ScreenFwd.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "gui/Gui.hpp"
-#include "gui/SpriteBatch.hpp"
-#include "rendering/TextureMap.hpp"
-#include "rendering/VertexFmt.hpp"
